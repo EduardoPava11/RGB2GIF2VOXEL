e3cde54ae38ac750cef5ecb5add44a3bb7ec9606
EduardoPava11 <maxypava@gmail.com>
2025-09-20 00:02:27 -0600

    Initial Commit

---
+++ RGB2GIF2VOXEL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RGB2GIF2VOXEL
+//
+//  Created by Daniel on 2025-09-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RGB2GIF2VOXEL/RGB2GIF2VOXELApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGB2GIF2VOXELApp.swift
+//  RGB2GIF2VOXEL
+//
+//  Created by Daniel on 2025-09-20.
+//
+
+import SwiftUI
+
+@main
+struct RGB2GIF2VOXELApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
